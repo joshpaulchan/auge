1a7df7e061ace5ac954ecd47ccd9fdd769cd4003
Joshua Paul Chan <joshpaulchan@gmail.com>
2019-11-30 12:52:02 -0500

    Specify high-level logic

--- src/api.py
+++ src/api.py
@@ -1,3 +1,6 @@
+import dataclasses
+import functools
+from typing import Sequence, Iterable, Mapping
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +9,44 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@dataclasses.dataclass
+class DetectedObject:
+    clas: str = dataclasses.field(default=None)
+    translations: Mapping = dataclasses.field(default_factory=dict)
+    confidence: float = dataclasses.field(default_factory=0.0)
+
+
+class ObjectDetector:
+    def detect(self, image) -> Iterable[DetectedObject]:
+        return []
+
+
+class Translator:
+    def translate(
+        self, text: str, output_language: str, input_language: str = "english"
+    ) -> str:
+        return text
+
+
+detector = ObjectDetector()
+translator = Translator()
+
+
+def attach_translation_to_obj(output_lang: str, obj: DetectedObject):
+    obj.translations.update({output_lang: translator.translate(obj.clas, output_lang)})
+    return obj
+
+
+@app.get("/detect")
+async def detect():
+    # get image + output languages from input
+    image = None
+    output = "german"
+
+    objects = detector.detect(image)
+    attach_translation = functools.partial(attach_translation_to_obj, output=output)
+    translated_objects = map(attach_translation, objects)
+
+    return {"objects": objects}
