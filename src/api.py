0a093864300e22a3ba567a612cdc75baf94002b2
Josh <joshpaulchan@users.noreply.github.com>
2019-11-30 21:11:13 -0500

    Feature: Image Recognition endpoint (#1)
    * use correct venv python
    
    * Specify high-level logic
    
    * extract domain to separate file
    
    * fix models
    
    * Fix DetectedObject api response

--- src/api.py
+++ src/api.py
@@ -1,4 +1,10 @@
+import functools
+from typing import Sequence, Iterable, Mapping
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from . import domain
 
 app = FastAPI()
 
@@ -6,3 +12,39 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+class DetectedObject(BaseModel):
+    clas: str
+    confidence: float
+    translations: Mapping
+
+    @classmethod
+    def from_model(cls, obj: domain.DetectedObject):
+        return cls(
+            clas=obj.clas, confidence=obj.confidence, translations=obj.translations
+        )
+
+
+class DetectionResponse(BaseModel):
+    objects: Sequence[DetectedObject]
+
+
+detector = domain.ObjectDetector()
+translator = domain.Translator()
+
+
+@app.get("/detect", response_model=DetectionResponse)
+async def detect(output: str):
+    # get image + output languages from input
+    image = None
+    output_lang = output
+
+    objects = []
+    for obj in detector.detect(image):
+        domain.attach_translation_to_obj(
+            obj, translator=translator, output_lang=output_lang
+        )
+        objects.append(DetectedObject.from_model(obj))
+
+    return {"objects": objects}
---
+++ src/domain.py
@@ -0,0 +1,30 @@
+import dataclasses
+import functools
+
+from typing import Sequence, Iterable, Mapping
+
+
+@dataclasses.dataclass
+class DetectedObject:
+    clas: str = dataclasses.field(default=None)
+    confidence: float = dataclasses.field(default=0.0)
+    translations: Mapping = dataclasses.field(default_factory=dict)
+
+
+class ObjectDetector:
+    def detect(self, image) -> Iterable[DetectedObject]:
+        return []
+
+
+class Translator:
+    def translate(
+        self, text: str, output_language: str, input_language: str = "english"
+    ) -> str:
+        return text
+
+
+def attach_translation_to_obj(
+    obj: DetectedObject, translator: Translator, output_lang: str
+):
+    obj.translations.update({output_lang: translator.translate(obj.clas, output_lang)})
+    return obj
