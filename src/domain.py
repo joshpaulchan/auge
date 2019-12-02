f182a43c277bd47404250fe0d543c8ddaf77d52a
Joshua Paul Chan <joshpaulchan@gmail.com>
2019-12-01 23:07:34 -0500

    Add prediction code

--- src/domain.py
+++ src/domain.py
@@ -5,6 +5,7 @@ import pathlib
 from fastai import vision
 
 from typing import Sequence, Iterable, Mapping
+import operator
 
 
 @dataclasses.dataclass
@@ -22,10 +23,26 @@ class ObjectDetector:
     def __init__(self, path_to_model: pathlib.Path):
         self.model = self._init_model(path_to_model)
 
-    def detect(self, image: bytes) -> Iterable[DetectedObject]:
+    def detect(self, image: bytes, min_conf: float = 0.85) -> Iterable[DetectedObject]:
         if not image:
             return []
-        return []
+
+        _, _, losses = self.model.predict(vision.open_image(image))
+
+        meets_min_confidence = functools.partial(operator.le, min_conf)
+        predictions = sorted(
+            filter(
+                lambda p: meets_min_confidence(p[1]),
+                zip(self.model.data.classes, map(float, losses)),
+            ),
+            key=lambda p: p[1],
+            reverse=True,
+        )
+
+        return [
+            DetectedObject.from_prediction(cls, confidence)
+            for cls, confidence in predictions
+        ]
 
     def _init_model(self, path_to_model: pathlib.Path):
         return vision.load_learner(path_to_model.parent, path_to_model.name)
