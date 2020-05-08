f80548e2f3479271ada4bc6a9d82cb0f0aae5a6e
Joshua Paul Chan <joshpaulchan@gmail.com>
2020-05-07 23:18:57 -0400

    clean up build tag

--- docker_build_tag.sh
+++ docker_build_tag.sh
@@ -3,8 +3,8 @@
 
 if [ -z "$1" ]
 then
-  echo "Usage: `basename $0` <PROJECT_ID> <TAG>"
-  exit $E_ARG_ERR
+  echo "Usage: $(basename "$0") <PROJECT_ID> <TAG>"
+  exit "$E_ARG_ERR"
 fi
 
 PROJECT_ID=$1
@@ -12,12 +12,12 @@ TAG=$2
 
 # function maps '/' in branch names to '-'
 function escape_tag() {
-    echo $(tr '/' '-' <<< "$1")
+    tr '/' '-' <<< "$1"
     return 0
 }
 
 # TODO: Detect if tag is a semver tag or other name
-ESCAPED_TAG=$(escape_tag $TAG)
+ESCAPED_TAG=$(escape_tag "$TAG")
 echo "Tagging '$PROJECT_ID' with: $ESCAPED_TAG."
-docker build --rm -t $PROJECT_ID:$ESCAPED_TAG .
+docker build --rm -t "$PROJECT_ID":"$ESCAPED_TAG" .
 # docker push $PROJECT_ID:$ESCAPED_TAG
